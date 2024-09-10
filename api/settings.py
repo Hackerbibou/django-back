b0e4480b68963f11fc047e13eff2b2e31ff05f8c
Habib <79129877+Hackerbibou@users.noreply.github.com>
2024-09-10 03:28:47 +0000

    test

--- api/settings.py
+++ api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=cldztbc4jg&xl0!x673!*v2_=p$$eu)=7*f#d0#zs$44xx-h^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','127.0.0.1:8000', '.vercel.app', 'localhost', '.github.dev','django-back-three.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1','127.0.0.1:8000', '127.0.0.1:3000','https://automatic-couscous-4r6775pxv56hjwv7-3000.app.github.dev','.app.github.dev', 'automatic-couscous-4r6775pxv56hjwv7-3000', '.vercel.app', 'localhost', 'localhost:3000' '.github.dev','django-back-three.vercel.app','minichart.vercel.app']
 
 
 # Application definition
