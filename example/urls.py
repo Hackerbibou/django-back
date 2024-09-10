10158cf2cade7fc9847d6a97690797f2448df192
Habib <79129877+Hackerbibou@users.noreply.github.com>
2024-09-10 02:11:54 +0000

    ff

--- example/urls.py
+++ example/urls.py
@@ -13,9 +13,6 @@ urlpatterns = [
     path("api/line-chart-data/", views.linechart, name='linechart'),
     path("api/bar-chart-data/", views.barchart, name='barchart'),
     path("api/pie-chart-data/", views.piechart, name='piechart'),
-    # path("__reload__/", include("django_browser_reload.urls")),
 ]
 
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
