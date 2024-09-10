93e4983e808ee27486c4905b4879abd30082cc37
Habib <79129877+Hackerbibou@users.noreply.github.com>
2024-09-10 01:44:29 +0000

    ff

--- example/urls.py
+++ example/urls.py
@@ -1,9 +1,14 @@
 # example/urls.py
 from django.urls import path
+from . import views
 
 from example.views import index
 
 
 urlpatterns = [
     path('', index),
+    path("api/candlestick-data/", views.candlestick, name='candlestick'),
+    path("api/line-chart-data/", views.linechart, name='linechart'),
+    path("api/bar-chart-data/", views.barchart, name='barchart'),
+    path("api/pie-chart-data/", views.piechart, name='piechart'),
 ]
--- example/views.py
+++ example/views.py
@@ -1,16 +1,26 @@
 # example/views.py
 from datetime import datetime
-
+from django.http import JsonResponse
+from .data import *
 from django.http import HttpResponse
+from .data import *
 
 def index(request):
-    now = datetime.now()
-    html = f'''
-    <html>
-        <body>
-            <h1>Hello from Vercel!</h1>
-            <p>The current time is { now }.</p>
-        </body>
-    </html>
-    '''
-    return HttpResponse(html)
+    context = {
+        "title": "Django example",
+    }
+    return render(request, "index.html", context)
+
+def candlestick(request):
+    return JsonResponse(data)
+
+def linechart(request):
+    return JsonResponse(data1)
+
+def barchart(request):
+
+    return JsonResponse(data2)
+
+def piechart(request):
+    
+   return JsonResponse(data3)
